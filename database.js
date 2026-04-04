c578b8dfd1a80a0b107fc9fde557f55d9b1fb116
sheddzy <shedrackebete@gmail.com>
2026-04-04 07:52:39 +0100

    Update database.js

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const Database = require('better-sqlite3');
 const bcrypt = require('bcryptjs');
 const path = require('path');
 
-const db = new Database(path.join('/app/data', 'nextforge.db'));
+const db = new Database(path.join('/app/data', 'nectforge.db'));
 db.pragma('journal_mode = WAL');
 
 db.exec(`
